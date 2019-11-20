74ad1dfe3f5658678d2308e6e3e197cff30711dd
Corsenac Pierre <pierre.corsenac@epitech.eu>
2019-11-20 14:52:53 -0500

    [SCRIPT] : adding the currency pairs algorythm and the prompting the result

--- pycurrencypairs.py
+++ pycurrencypairs.py
@@ -1,4 +1,37 @@
+from forex_python.converter import CurrencyRates
+import sys
+
+def prompt_rates_for_one_currency(rates, base_currency, currencies):
+    print("\n%s :\n" % base_currency)
+    for currency in currencies:
+        try:
+            print("%s %s" % (currency, rates.get_rate(base_currency, currency)))
+        except:
+            print("N/A")
+
+def get_currencies(rates):
+    currencies = []
+    for argument in sys.argv:
+        if len(argument) == 3:
+            try:
+                rates.get_rates(argument)
+                currencies.append(argument)
+            except:
+                continue
+    
+    print('----')
+    print(currencies)
+    print('----')
+    return currencies
+
+def get_exchange_rate(currencies, rates):
+    for currency in currencies:
+        prompt_rates_for_one_currency(rates, currency, currencies)
+          
 def main():
-    print("main")
+    rates = CurrencyRates()
+    currencies = get_currencies(rates)
+
+    get_exchange_rate(currencies, rates)
 
 main()
